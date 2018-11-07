5acd5f46657827944d36f6a5162b343c067c11e8
sjaqjwor <lsklsk45@naver.com>
2018-11-07 15:58:51 +0900

    apply .gitignore

--- src/main/java/com/movie/movie/configs/MovieSchedule.java
+++ src/main/java/com/movie/movie/configs/MovieSchedule.java
@@ -1,4 +1,9 @@
 package com.movie.movie.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class MovieSchedule {
+
+    @S
+
 }
--- src/main/java/com/movie/movie/domains/DailyMovie.java
+++ src/main/java/com/movie/movie/domains/DailyMovie.java
@@ -1,4 +1,15 @@
 package com.movie.movie.domains;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class DailyMovie {
+
+    private int rank;
+    private int accaudience;
+    private int dailyaudience;
+    private Date date;
+    private int id;
 }
